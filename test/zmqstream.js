0664b5f5641b652450e3b1da97f7391dfe6e541d
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-02-14 09:10:02 -0800

    Added more messaging (PAIR) tests.

--- test/zmqstream.js
+++ test/zmqstream.js
@@ -2,6 +2,10 @@
 var zmqstream = require('../lib/zmqstream')
   , expect = require('chai').expect
 
+function getInprocEndpoint() {
+  return 'inproc://zmqstreamtest' + Math.random().toString().slice(2, 6)
+}
+
 describe('ZMQStream', function () {
   it('should exist', function () {
     expect(zmqstream).to.exist
@@ -79,6 +83,24 @@ describe('ZMQStream', function () {
     })
 
     describe('write', function () {
+      beforeEach(function () {
+        var self = this
+
+        self.socket = new zmqstream.Socket()
+      })
+
+      it('should be callable without error with 0 frames', function () {
+        this.socket.write([])
+      })
+
+      it('should be callable without error with 1 frame', function () {
+        this.socket.write([new Buffer('test')])
+      })
+
+      it('should be callable without error with n frames', function () {
+        this.socket.write([new Buffer('one'), new Buffer('two'), new Buffer('three')])
+      })
+
       it('should throw if the Socket is closed', function () {
         var socket = new zmqstream.Socket({
           type: zmqstream.Type.REQ
@@ -93,6 +115,86 @@ describe('ZMQStream', function () {
     })
 
     describe('read', function () {
+      beforeEach(function () {
+        var self = this
+
+        self.socket = new zmqstream.Socket()
+        self.endpoint = getInprocEndpoint()
+      })
+
+      it('should be callable without error with 0 frames', function () {
+        this.socket.read(0)
+      })
+
+      it('should be callable without error with 1 frame', function () {
+        this.socket.read(1)
+      })
+
+      it('should be callable without error with n frames', function () {
+        this.socket.read(3)
+      })
+
+      it('should receive the frames of a single message in order', function () {
+        var sender = new zmqstream.Socket()
+          , message = [new Buffer('one'), new Buffer('two')]
+
+        sender.bind(this.endpoint)
+        this.socket.connect(this.endpoint)
+
+        sender.write(message)
+        message = this.socket.read()
+
+        expect(message).to.exist
+        expect(message).to.be.an.instanceof(Array)
+        expect(message).to.have.length(1)
+        expect(message[0]).to.be.an.instanceof(Array)
+        expect(message[0]).to.have.length(2)
+        expect(message[0][0]).to.be.an.instanceof(Buffer)
+        expect(message[0][0]).to.have.length(3)
+        expect(message[0][0].toString()).to.equal('one')
+        expect(message[0][1]).to.be.an.instanceof(Buffer)
+        expect(message[0][1]).to.have.length(3)
+        expect(message[0][1].toString()).to.equal('two')
+      })
+
+      it('should receive multiple messages in order', function () {
+        var sender = new zmqstream.Socket()
+          , messages = [
+              [new Buffer('one'), new Buffer('two')],
+              [new Buffer('three'), new Buffer('four'), new Buffer('five')]
+            ]
+
+        sender.bind(this.endpoint)
+        this.socket.connect(this.endpoint)
+
+        sender.write(messages[0])
+        sender.write(messages[1])
+        messages = this.socket.read()
+
+        // Two messages
+        expect(messages).to.exist
+        expect(messages).to.be.an.instanceof(Array)
+        expect(messages).to.have.length(2)
+
+        // Two frames (one, two) in the first message
+        expect(messages[0]).to.be.an.instanceof(Array)
+        expect(messages[0]).to.have.length(2)
+        expect(messages[0][0]).to.be.an.instanceof(Buffer)
+        expect(messages[0][0].toString()).to.equal('one')
+        expect(messages[0][1]).to.be.an.instanceof(Buffer)
+        expect(messages[0][1].toString()).to.equal('two')
+
+        // Three frames (three, four, fice) in the second message
+        expect(messages[1]).to.be.an.instanceof(Array)
+        expect(messages[1]).to.have.length(3)
+        expect(messages[1][0]).to.be.an.instanceof(Buffer)
+        expect(messages[1][0].toString()).to.equal('three')
+        expect(messages[1][1]).to.be.an.instanceof(Buffer)
+        expect(messages[1][1].toString()).to.equal('four')
+        expect(messages[1][2]).to.be.an.instanceof(Buffer)
+        expect(messages[1][2].toString()).to.equal('five')
+      })
+
       it('should throw if the Socket is closed', function () {
         var socket = new zmqstream.Socket({
           type: zmqstream.Type.REQ
@@ -106,63 +208,79 @@ describe('ZMQStream', function () {
       })
     })
 
-    describe('PAIR', function () {
+    describe('bind', function () {
       beforeEach(function () {
-        var self = this
-
-        self.socket = new zmqstream.Socket({
-          type: zmqstream.Type.PAIR
-        })
-        self.endpoint = 'inproc:///tmp/zmqstreamtest' + Math.random().toString().slice(2, 6)
+        this.socket = new zmqstream.Socket()
+        this.endpoint = getInprocEndpoint()
       })
 
-      it('should be able to bind without error', function () {
+      it('should be callable without error', function () {
         this.socket.bind(this.endpoint)
       })
 
-      it('should be able to connect without error', function () {
+      it('should throw if no endpoint is provided', function () {
+        var self = this
+
+        expect(function () {
+          self.socket.bind()
+        }).to.throw('No endpoint')
+      })
+    })
+
+    describe('connect', function () {
+      beforeEach(function () {
+        this.socket = new zmqstream.Socket()
+        this.endpoint = getInprocEndpoint()
+      })
+
+      it('should be callable without error', function () {
         this.socket.connect(this.endpoint)
       })
 
-      it('should be able to unbind without error', function () {
+      it('should throw if no endpoint is provided', function () {
+        var self = this
+
+        expect(function () {
+          self.socket.connect()
+        }).to.throw('No endpoint')
+      })
+    })
+
+    describe('unbind', function () {
+      beforeEach(function () {
+        this.socket = new zmqstream.Socket()
+        this.endpoint = getInprocEndpoint()
+      })
+
+      it('should be callable without error', function () {
         this.socket.unbind(this.endpoint)
       })
 
-      it('should be able to disconnect without error', function () {
+      it('should throw if no endpoint is provided', function () {
+        var self = this
+
+        expect(function () {
+          self.socket.unbind()
+        }).to.throw('No endpoint')
+      })
+    })
+
+    describe('disconnect', function () {
+      beforeEach(function () {
+        this.socket = new zmqstream.Socket()
+        this.endpoint = getInprocEndpoint()
+      })
+
+      it('should be callable without error', function () {
         this.socket.disconnect(this.endpoint)
       })
 
-      it('should be able to write messages without error', function () {
-        this.socket.write([new Buffer('test')])
-      })
+      it('should throw if no endpoint is provided', function () {
+        var self = this
 
-      it('should be able to read messages without error', function () {
-        this.socket.read(1)
-      })
-
-      // TODO: Multiple messages, not multiple frames.
-      it('should be able to read the messages sent', function () {
-        var a = new zmqstream.Socket()
-          , b = new zmqstream.Socket()
-          , message = [new Buffer('one'), new Buffer('two')]
-
-        a.bind(this.endpoint)
-        b.connect(this.endpoint)
-
-        a.write(message)
-        message = b.read()
-
-        expect(message).to.exist
-        expect(message).to.be.an.instanceof(Array)
-        expect(message).to.have.length(1)
-        expect(message[0]).to.be.an.instanceof(Array)
-        expect(message[0]).to.have.length(2)
-        expect(message[0][0]).to.be.an.instanceof(Buffer)
-        expect(message[0][0]).to.have.length(3)
-        expect(message[0][0].toString()).to.equal('one')
-        expect(message[0][1]).to.be.an.instanceof(Buffer)
-        expect(message[0][1]).to.have.length(3)
-        expect(message[0][1].toString()).to.equal('two')
+        expect(function () {
+          self.socket.disconnect()
+        }).to.throw('No endpoint')
       })
     })
 
