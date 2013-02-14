c4bfcc02d6baec5e84db6df85b439ec8879fef7a
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-02-13 16:29:04 -0800

    Test stubs.

--- test/zmqstream.js
+++ test/zmqstream.js
@@ -3,7 +3,17 @@ var zmqstream = require('../lib/zmqstream')
   , expect = require('chai').expect
 
 describe('ZMQStream', function () {
+  it('should exist', function () {
+    expect(zmqstream).to.exist
+    expect(zmqstream).to.be.an('object')
+  })
+
   describe('Constants', function () {
+    it('should exist', function () {
+      expect(zmqstream.Type).to.exist
+      expect(zmqstream.Type).to.be.an('object')
+    })
+
     it('should export all socket types', function () {
       expect(zmqstream.Type.REQ, 'REQ').to.exist
       expect(zmqstream.Type.REP, 'REP').to.exist
@@ -16,6 +26,29 @@ describe('ZMQStream', function () {
       expect(zmqstream.Type.PUSH, 'PUSH').to.exist
       expect(zmqstream.Type.PULL, 'PULL').to.exist
       expect(zmqstream.Type.PAIR, 'PAIR').to.exist
+    })
+  })
+
+  describe('Socket', function () {
+    it('should exist', function () {
+      expect(zmqstream.Socket).to.exist
+      expect(zmqstream.Socket).to.be.a('function')
+    })
+
+    it('should throw if no type is provided')
+    it('should be constructed without error with a type')
+
+    describe('write', function () {
+      it('should throw if the Socket is closed')
+    })
+
+    describe('read', function () {
+      it('should throw if the Socket is closed')
+    })
+
+    describe('REQ-REP', function () {
+      it('should be able to write messages without error')
+      it('should be able to read messages without error')
     })
   })
 })
